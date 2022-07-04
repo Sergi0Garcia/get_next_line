2486a01169d09d4475c718bd43b30491118a6962
Sergio Garcia <segarcia@student.42heilbronn.de>
2022-07-04 13:48:05 +0200

    Ckpt

--- get_next_line.c
+++ get_next_line.c
@@ -6,19 +6,20 @@
 /*   By: segarcia <segarcia@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/04 09:11:35 by segarcia          #+#    #+#             */
-/*   Updated: 2022/07/04 11:37:48 by segarcia         ###   ########.fr       */
+/*   Updated: 2022/07/04 13:39:03 by segarcia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
+#include <string.h>
 
 int	check_current_stash(char *stash)
 {
-	int i;
+	int	i;
 
 	i = 0;
-	while(stash && stash[i])
+	while (stash && stash[i])
 	{
 		if (stash[i] == 10)
 			return (i);
@@ -29,13 +30,14 @@ int	check_current_stash(char *stash)
 
 char	*get_next_line(int fd)
 {
-	char			*line = NULL;
+	char			*line;
 	char			buf[BUFFER_SIZE + 1];
 	static char		*stash = "";
 	char			*tmp;
 	int				ret;
 	int				brk_line;
-	int 			real_brkline;
+	int				real_brkline;
+
 
 	brk_line = -1;
 	ret = BUFFER_SIZE;
@@ -47,11 +49,11 @@ char	*get_next_line(int fd)
 		ret = read(fd, buf, BUFFER_SIZE);
 		if (ret == -1)
 			return (NULL);
-		if (!ret)
+		if (ret == 0)
 		{
 			if (!ft_strlen(stash))
 				return (NULL);
-			break;
+			break ;
 		}
 		buf[ret] = 0;
 		tmp = stash;
@@ -74,8 +76,8 @@ char	*get_next_line(int fd)
 // 	char		*res;
 // 	int			fd;
 
-// 	// fd = open("./gnlTester/files/empty", O_RDONLY);
-// 	fd = open("./tmp.txt", O_RDONLY);
+// 	fd = open("./gnlTester/files/nl", O_RDONLY);
+// 	// fd = open("./tmp.txt", O_RDONLY);
 // 	res = get_next_line(fd);
 // 	printf("output 1 = %s-\n", res);
 // 	res = get_next_line(fd);
@@ -86,5 +88,6 @@ char	*get_next_line(int fd)
 // 	printf("output 2 = %s-\n", res);
 // 	res = get_next_line(fd);
 // 	printf("output 2 = %s-\n", res);
+// 	system("leaks a.out");
 // 	return (0);
 // }
